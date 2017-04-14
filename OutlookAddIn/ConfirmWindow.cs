ff65d609c87ac5ed18f242831000e0a474e04707
T.Miyake <info@miyake.me>
2017-04-15 00:43:18 +0900

    Add comments.
    Add comments.

--- OutlookAddIn/ConfirmWindow.cs
+++ OutlookAddIn/ConfirmWindow.cs
@@ -8,7 +8,10 @@ namespace OutlookAddIn
 {
     public partial class ConfirmWindow : Form
     {
-
+        /// <summary>
+        /// メール送信の確認画面を表示。
+        /// </summary>
+        /// <param name="mail">送信するメールに関する情報</param>
         public ConfirmWindow(Outlook._MailItem mail)
         {
             InitializeComponent();
@@ -16,15 +19,23 @@ namespace OutlookAddIn
             GetRecipient(mail);
         }
 
+        /// <summary>
+        /// 送信先メールアドレスを取得し、画面に表示する。
+        /// </summary>
+        /// <param name="mail">送信するメールに関する情報</param>
         public void GetRecipient(Outlook._MailItem mail)
         {
             var displayNameAndRecipient = new Dictionary<string, string>();
 
             foreach (Outlook.Recipient recip in mail.Recipients)
             {
+                // Exchangeの連絡先に登録された情報を取得。
                 var exchangedUser = recip.AddressEntry.GetExchangeUser();
+
+                // ローカルの連絡先に登録された情報を取得。
                 var registeredUser = recip.AddressEntry.GetContact();
 
+                // 登録されたメールアドレスの場合、登録名のみが表示されるため、メールアドレスと共に表示されるよう表示用テキストを生成。
                 var nameAndMailAddress = exchangedUser != null
                     ? exchangedUser.Name + @" (" + exchangedUser.PrimarySmtpAddress + @")"
                     : registeredUser != null
@@ -34,10 +45,12 @@ namespace OutlookAddIn
                 displayNameAndRecipient[recip.Name] = nameAndMailAddress;
             }
 
+            // 宛先(To,CC,BCC)に登録された宛先又は登録名を配列に格納。
             var toAdresses = mail.To?.Split(';') ?? new string[] { };
             var ccAdresses = mail.CC?.Split(';') ?? new string[] { };
             var bccAdresses = mail.BCC?.Split(';') ?? new string[] { };
 
+            // 宛先や登録名から、表示用テキスト(メールアドレスや登録名)を各エリアに表示。
             foreach (var i in displayNameAndRecipient)
             {
                 if (toAdresses.Any(address => address.Contains(i.Key)))
@@ -66,6 +79,9 @@ namespace OutlookAddIn
             SendButtonSwitch();
         }
 
+        /// <summary>
+        /// 全てのチェックボックスにチェックされた場合のみ、送信ボタンを有効とする。
+        /// </summary>
         private void SendButtonSwitch()
         {
             if (ToAddressList.CheckedItems.Count == ToAddressList.Items.Count && CcAddressList.CheckedItems.Count == CcAddressList.Items.Count && BccAddressList.CheckedItems.Count == BccAddressList.Items.Count)
