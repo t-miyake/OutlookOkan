231a7fa8afe9a0aa593108f01b9d3ecc8be4bb16
T.Miyake <info@miyake.me>
2017-04-17 21:19:33 +0900

    Change title.

--- OutlookAddIn/ConfirmWindow.Designer.cs
+++ OutlookAddIn/ConfirmWindow.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.sendButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
-            this.ToAddressList = new CustomCheckedListBox();
+            this.ToAddressList = new OutlookAddIn.CustomCheckedListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.CcAddressList = new CustomCheckedListBox();
+            this.CcAddressList = new OutlookAddIn.CustomCheckedListBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.BccAddressList = new CustomCheckedListBox();
+            this.BccAddressList = new OutlookAddIn.CustomCheckedListBox();
             this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -44,9 +44,10 @@
             // 
             this.sendButton.DialogResult = System.Windows.Forms.DialogResult.Yes;
             this.sendButton.Enabled = false;
-            this.sendButton.Location = new System.Drawing.Point(552, 515);
+            this.sendButton.Location = new System.Drawing.Point(442, 429);
+            this.sendButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(104, 36);
+            this.sendButton.Size = new System.Drawing.Size(83, 30);
             this.sendButton.TabIndex = 0;
             this.sendButton.Text = "送信";
             this.sendButton.UseVisualStyleBackColor = true;
@@ -54,9 +55,10 @@
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(680, 515);
+            this.cancelButton.Location = new System.Drawing.Point(544, 429);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(104, 36);
+            this.cancelButton.Size = new System.Drawing.Size(83, 30);
             this.cancelButton.TabIndex = 1;
             this.cancelButton.Text = "キャンセル";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -64,83 +66,91 @@
             // ToAddressList
             // 
             this.ToAddressList.FormattingEnabled = true;
-            this.ToAddressList.Location = new System.Drawing.Point(35, 109);
+            this.ToAddressList.Location = new System.Drawing.Point(28, 91);
+            this.ToAddressList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ToAddressList.Name = "ToAddressList";
             this.ToAddressList.ScrollAlwaysVisible = true;
-            this.ToAddressList.Size = new System.Drawing.Size(723, 84);
+            this.ToAddressList.Size = new System.Drawing.Size(579, 55);
             this.ToAddressList.TabIndex = 3;
             this.ToAddressList.SelectedIndexChanged += new System.EventHandler(this.ToAddressList_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 19);
+            this.label1.Location = new System.Drawing.Point(26, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 18);
+            this.label1.Size = new System.Drawing.Size(186, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "本当にメールを送信しますか？";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 46);
+            this.label2.Location = new System.Drawing.Point(26, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(475, 18);
+            this.label2.Size = new System.Drawing.Size(394, 15);
             this.label2.TabIndex = 5;
             this.label2.Text = "すべてのアドレスを確認し、チェックを入れて、送信を押してください。";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 83);
+            this.label3.Location = new System.Drawing.Point(26, 69);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 18);
+            this.label3.Size = new System.Drawing.Size(24, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "To";
             // 
             // CcAddressList
             // 
             this.CcAddressList.FormattingEnabled = true;
-            this.CcAddressList.Location = new System.Drawing.Point(35, 241);
+            this.CcAddressList.Location = new System.Drawing.Point(28, 201);
+            this.CcAddressList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CcAddressList.Name = "CcAddressList";
             this.CcAddressList.ScrollAlwaysVisible = true;
-            this.CcAddressList.Size = new System.Drawing.Size(723, 84);
+            this.CcAddressList.Size = new System.Drawing.Size(579, 55);
             this.CcAddressList.TabIndex = 8;
             this.CcAddressList.SelectedIndexChanged += new System.EventHandler(this.CcAddressList_SelectedIndexChanged_1);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 214);
+            this.label4.Location = new System.Drawing.Point(26, 178);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 18);
+            this.label4.Size = new System.Drawing.Size(27, 15);
             this.label4.TabIndex = 9;
             this.label4.Text = "CC";
             // 
             // BccAddressList
             // 
             this.BccAddressList.FormattingEnabled = true;
-            this.BccAddressList.Location = new System.Drawing.Point(35, 382);
+            this.BccAddressList.Location = new System.Drawing.Point(28, 318);
+            this.BccAddressList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BccAddressList.Name = "BccAddressList";
             this.BccAddressList.ScrollAlwaysVisible = true;
-            this.BccAddressList.Size = new System.Drawing.Size(723, 84);
+            this.BccAddressList.Size = new System.Drawing.Size(579, 55);
             this.BccAddressList.TabIndex = 10;
             this.BccAddressList.SelectedIndexChanged += new System.EventHandler(this.BccAddressList_SelectedIndexChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(32, 355);
+            this.label5.Location = new System.Drawing.Point(26, 296);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 18);
+            this.label5.Size = new System.Drawing.Size(37, 15);
             this.label5.TabIndex = 11;
             this.label5.Text = "BCC";
             // 
             // ConfirmWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 565);
+            this.ClientSize = new System.Drawing.Size(640, 471);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.BccAddressList);
             this.Controls.Add(this.label4);
@@ -151,8 +161,10 @@
             this.Controls.Add(this.ToAddressList);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.sendButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ConfirmWindow";
-            this.Text = "ConfirmWindow";
+            this.ShowIcon = false;
+            this.Text = "メール送信前の確認です。";
             this.ResumeLayout(false);
             this.PerformLayout();
 
