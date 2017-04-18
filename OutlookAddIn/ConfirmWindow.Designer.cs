097bba37f0c9fa61e627e1001c4a3eabcea67e01
T.Miyake <info@miyake.me>
2017-04-18 13:38:29 +0900

    Add New function.
    下記機能を実装。
    登録された名称とドメインから、宛先候補ではないアドレスが宛先に含まれている場合に、警告を表示する。
    名称とドメインの登録は設定画面から可能。

--- OutlookAddIn/ConfirmWindow.Designer.cs
+++ OutlookAddIn/ConfirmWindow.Designer.cs
@@ -38,16 +38,17 @@
             this.label4 = new System.Windows.Forms.Label();
             this.BccAddressList = new OutlookAddIn.CustomCheckedListBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.AlertBox = new OutlookAddIn.CustomCheckedListBox();
             this.SuspendLayout();
             // 
             // sendButton
             // 
             this.sendButton.DialogResult = System.Windows.Forms.DialogResult.Yes;
             this.sendButton.Enabled = false;
-            this.sendButton.Location = new System.Drawing.Point(442, 429);
+            this.sendButton.Location = new System.Drawing.Point(557, 545);
             this.sendButton.Margin = new System.Windows.Forms.Padding(2);
             this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(83, 30);
+            this.sendButton.Size = new System.Drawing.Size(104, 36);
             this.sendButton.TabIndex = 0;
             this.sendButton.Text = "送信";
             this.sendButton.UseVisualStyleBackColor = true;
@@ -55,10 +56,10 @@
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(544, 429);
+            this.cancelButton.Location = new System.Drawing.Point(685, 545);
             this.cancelButton.Margin = new System.Windows.Forms.Padding(2);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(83, 30);
+            this.cancelButton.Size = new System.Drawing.Size(104, 36);
             this.cancelButton.TabIndex = 1;
             this.cancelButton.Text = "キャンセル";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -66,91 +67,102 @@
             // ToAddressList
             // 
             this.ToAddressList.FormattingEnabled = true;
-            this.ToAddressList.Location = new System.Drawing.Point(28, 91);
+            this.ToAddressList.Location = new System.Drawing.Point(35, 243);
             this.ToAddressList.Margin = new System.Windows.Forms.Padding(2);
             this.ToAddressList.Name = "ToAddressList";
             this.ToAddressList.ScrollAlwaysVisible = true;
-            this.ToAddressList.Size = new System.Drawing.Size(579, 55);
+            this.ToAddressList.Size = new System.Drawing.Size(723, 64);
             this.ToAddressList.TabIndex = 3;
             this.ToAddressList.SelectedIndexChanged += new System.EventHandler(this.ToAddressList_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 16);
+            this.label1.Location = new System.Drawing.Point(32, 19);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 15);
+            this.label1.Size = new System.Drawing.Size(225, 18);
             this.label1.TabIndex = 4;
             this.label1.Text = "本当にメールを送信しますか？";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 38);
+            this.label2.Location = new System.Drawing.Point(32, 46);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(394, 15);
+            this.label2.Size = new System.Drawing.Size(475, 18);
             this.label2.TabIndex = 5;
             this.label2.Text = "すべてのアドレスを確認し、チェックを入れて、送信を押してください。";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 69);
+            this.label3.Location = new System.Drawing.Point(32, 217);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(24, 15);
+            this.label3.Size = new System.Drawing.Size(28, 18);
             this.label3.TabIndex = 6;
             this.label3.Text = "To";
             // 
             // CcAddressList
             // 
             this.CcAddressList.FormattingEnabled = true;
-            this.CcAddressList.Location = new System.Drawing.Point(28, 201);
+            this.CcAddressList.Location = new System.Drawing.Point(35, 351);
             this.CcAddressList.Margin = new System.Windows.Forms.Padding(2);
             this.CcAddressList.Name = "CcAddressList";
             this.CcAddressList.ScrollAlwaysVisible = true;
-            this.CcAddressList.Size = new System.Drawing.Size(579, 55);
+            this.CcAddressList.Size = new System.Drawing.Size(723, 64);
             this.CcAddressList.TabIndex = 8;
             this.CcAddressList.SelectedIndexChanged += new System.EventHandler(this.CcAddressList_SelectedIndexChanged_1);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(26, 178);
+            this.label4.Location = new System.Drawing.Point(32, 324);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(27, 15);
+            this.label4.Size = new System.Drawing.Size(32, 18);
             this.label4.TabIndex = 9;
             this.label4.Text = "CC";
             // 
             // BccAddressList
             // 
             this.BccAddressList.FormattingEnabled = true;
-            this.BccAddressList.Location = new System.Drawing.Point(28, 318);
+            this.BccAddressList.Location = new System.Drawing.Point(35, 464);
             this.BccAddressList.Margin = new System.Windows.Forms.Padding(2);
             this.BccAddressList.Name = "BccAddressList";
             this.BccAddressList.ScrollAlwaysVisible = true;
-            this.BccAddressList.Size = new System.Drawing.Size(579, 55);
+            this.BccAddressList.Size = new System.Drawing.Size(723, 64);
             this.BccAddressList.TabIndex = 10;
             this.BccAddressList.SelectedIndexChanged += new System.EventHandler(this.BccAddressList_SelectedIndexChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(26, 296);
+            this.label5.Location = new System.Drawing.Point(32, 437);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.Size = new System.Drawing.Size(43, 18);
             this.label5.TabIndex = 11;
             this.label5.Text = "BCC";
             // 
+            // AlertBox
+            // 
+            this.AlertBox.FormattingEnabled = true;
+            this.AlertBox.Location = new System.Drawing.Point(35, 121);
+            this.AlertBox.Name = "AlertBox";
+            this.AlertBox.ScrollAlwaysVisible = true;
+            this.AlertBox.Size = new System.Drawing.Size(723, 64);
+            this.AlertBox.TabIndex = 12;
+            this.AlertBox.SelectedIndexChanged += new System.EventHandler(this.AlertBox_SelectedIndexChanged);
+            // 
             // ConfirmWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 471);
+            this.ClientSize = new System.Drawing.Size(800, 592);
+            this.Controls.Add(this.AlertBox);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.BccAddressList);
             this.Controls.Add(this.label4);
@@ -183,5 +195,6 @@
         private System.Windows.Forms.Label label4;
         private CustomCheckedListBox BccAddressList;
         private System.Windows.Forms.Label label5;
+        private CustomCheckedListBox AlertBox;
     }
 }
--- OutlookAddIn/ConfirmWindow.cs
+++ OutlookAddIn/ConfirmWindow.cs
@@ -2,12 +2,17 @@
 using System;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using CsvHelper;
 
 namespace OutlookAddIn
 {
     public partial class ConfirmWindow : Form
     {
+        public Dictionary<string, string> DisplayNameAndRecipient = new Dictionary<string, string>();
+
         /// <summary>
         /// メール送信の確認画面を表示。
         /// </summary>
@@ -17,6 +22,45 @@ namespace OutlookAddIn
             InitializeComponent();
 
             DrawRecipient(mail);
+            CheckMailbodyAndRecipient(mail);
+        }
+
+        /// <summary>
+        /// 登録された名称とドメインから、宛先候補ではないアドレスが宛先に含まれている場合に、警告を表示する。
+        /// </summary>
+        /// <param name="mail"></param>
+        public void CheckMailbodyAndRecipient(Outlook._MailItem mail)
+        {
+            List<NameAndDomains> nameAndDomainsList;
+
+            using (var csvParser =
+                new CsvParser(new StreamReader(@"c:\test\test.csv", Encoding.GetEncoding("Shift_JIS"))))
+            {
+                csvParser.Configuration.HasHeaderRecord = false;
+                csvParser.Configuration.RegisterClassMap<NameAndDomainsMap>();
+
+                nameAndDomainsList = new CsvReader(csvParser).GetRecords<NameAndDomains>().ToList();
+            }
+
+            //メールの本文中に、登録された名称があるか確認。
+            var recipientCandidateNames = (from nameAnddomain in nameAndDomainsList where mail.Body.Contains(nameAnddomain.Name) select nameAnddomain.Name).ToList();
+            var recipientCandidateDomains = (from nameAnddomain in nameAndDomainsList where mail.Body.Contains(nameAnddomain.Name) select nameAnddomain.Domain).ToList();
+
+            ////登録された名称かつ本文中に登場した名称以外のドメインが宛先に含まれている場合、警告を表示
+            foreach (var recipients in DisplayNameAndRecipient)
+            {
+                if (recipientCandidateDomains.Any(domains => domains.Equals(recipients.Value.Substring(recipients.Value.IndexOf("@", StringComparison.Ordinal)))))
+                {
+                    //正常なのでとりあえず何もしない。
+                }
+                else
+                {
+                    //送信者ドメインは警告対象外。
+                    if (recipients.Value.Contains(mail.SendUsingAccount.SmtpAddress.Substring(mail.SendUsingAccount.SmtpAddress.IndexOf("@", StringComparison.Ordinal)))) continue;
+                    AlertBox.Items.Add(recipients.Key + @" : このアドレスは意図した宛先とは無関係の可能性があります！");
+                    AlertBox.ColorFlag.Add(true);
+                }
+            }
         }
 
         /// <summary>
@@ -25,8 +69,6 @@ namespace OutlookAddIn
         /// <param name="mail">送信するメールに関する情報</param>
         public void DrawRecipient(Outlook._MailItem mail)
         {
-            var displayNameAndRecipient = new Dictionary<string, string>();
-
             foreach (Outlook.Recipient recip in mail.Recipients)
             {
                 // Exchangeの連絡先に登録された情報を取得。
@@ -42,7 +84,7 @@ namespace OutlookAddIn
                         ? recip.Name
                         : recip.Address;
 
-                displayNameAndRecipient[recip.Name] = nameAndMailAddress;
+                DisplayNameAndRecipient[recip.Name] = nameAndMailAddress;
             }
 
             // 宛先(To,CC,BCC)に登録された宛先又は登録名を配列に格納。
@@ -54,7 +96,7 @@ namespace OutlookAddIn
 
             // 宛先や登録名から、表示用テキスト(メールアドレスや登録名)を各エリアに表示。
             // 宛先ドメインが送信元ドメインと異なる場合、色を変更するフラグをtrue、そうでない場合falseとする。
-            foreach (var i in displayNameAndRecipient)
+            foreach (var i in DisplayNameAndRecipient)
             {
                 if (toAdresses.Any(address => address.Contains(i.Key)))
                 {
@@ -74,6 +116,10 @@ namespace OutlookAddIn
                     BccAddressList.ColorFlag.Add(!i.Value.Contains(senderDomain));
                 }
             }
+        }
+        private void AlertBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SendButtonSwitch();
         }
 
         private void ToAddressList_SelectedIndexChanged(object sender, EventArgs e)
@@ -104,6 +150,5 @@ namespace OutlookAddIn
                 sendButton.Enabled = false;
             }
         }
-
     }
 }
