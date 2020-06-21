d0f637d517189e9cf97e22b4ea336d04d5608d3f
T_Miyake <t@miyake.me>
2020-06-21 22:31:38 +0900

    添付ファイル忘れの警告機能が動作していなかった問題を修正。

--- OutlookOkan/ThisAddIn.cs
+++ OutlookOkan/ThisAddIn.cs
@@ -159,8 +159,10 @@ namespace OutlookOkan
             if (generalSetting.Count == 0) return;
 
             _generalSetting.LanguageCode = generalSetting[0].LanguageCode;
+
             if (isLaunch) return;
 
+            _generalSetting.EnableForgottenToAttachAlert = generalSetting[0].EnableForgottenToAttachAlert;
             _generalSetting.IsDoNotConfirmationIfAllRecipientsAreSameDomain = generalSetting[0].IsDoNotConfirmationIfAllRecipientsAreSameDomain;
             _generalSetting.IsDoDoNotConfirmationIfAllWhite = generalSetting[0].IsDoDoNotConfirmationIfAllWhite;
             _generalSetting.IsAutoCheckIfAllRecipientsAreSameDomain = generalSetting[0].IsAutoCheckIfAllRecipientsAreSameDomain;
