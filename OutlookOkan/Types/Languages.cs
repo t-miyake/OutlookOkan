d0c8a40c3641f75c0f0b7df5afb400d9cfb8ebad
T.Miyake <t@miyake.me>
2021-09-27 10:22:05 +0900

    言語を追加。(中国語(簡体字)、中国語(繁体字)、スペイン語、ヒンディー語、ロシア語、ドイツ語、韓国語、タイ語。)

--- OutlookOkan/Types/Languages.cs
+++ OutlookOkan/Types/Languages.cs
@@ -8,8 +8,16 @@ namespace OutlookOkan.Types
 
         public Languages()
         {
-            Language.Add(new LanguageCodeAndName { LanguageNumber = 0, LanguageName = "日本語", LanguageCode = "ja-JP" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 0, LanguageName = "日本語 [Japanese]", LanguageCode = "ja-JP" });
             Language.Add(new LanguageCodeAndName { LanguageNumber = 1, LanguageName = "English", LanguageCode = "en-US" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 2, LanguageName = "简体中文 [Chinese (Simplified, China)] Beta", LanguageCode = "zh-CN" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 3, LanguageName = "繁體中文 [Chinese (Traditional, Taiwan)] Beta", LanguageCode = "zh-TW" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 4, LanguageName = "Español [Spanish] Beta", LanguageCode = "es-ES" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 5, LanguageName = "हिन्दी [Hindi] Beta", LanguageCode = "hi-IN" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 6, LanguageName = "Русский язык [Russian] Beta", LanguageCode = "ru-RU" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 7, LanguageName = "Deutsch [German] Beta", LanguageCode = "de-DE" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 8, LanguageName = "한국어 [Korean] Beta", LanguageCode = "ko-KR" });
+            Language.Add(new LanguageCodeAndName { LanguageNumber = 9, LanguageName = "ไทย [Thai] Beta", LanguageCode = "th-TH" });
         }
     }
 
