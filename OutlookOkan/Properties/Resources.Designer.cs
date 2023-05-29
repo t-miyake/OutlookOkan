f72bc26dda93b768ea8e546c87fbfade66423b1c
T.Miyake <t@miyake.me>
2023-05-29 09:19:11 +0900

    バージョン番号を更新。

--- OutlookOkan/Properties/Resources.Designer.cs
+++ OutlookOkan/Properties/Resources.Designer.cs
@@ -2324,7 +2324,7 @@ namespace OutlookOkan.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Version 2.8.0.1.1.
+        ///   Looks up a localized string similar to Version 2.8.0.1.2.
         /// </summary>
         public static string Version {
             get {
